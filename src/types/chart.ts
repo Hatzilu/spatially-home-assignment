e981938e6d816e1db17f9c659f89623715fc1638
Hatzilu <taazrooli@gmail.com>
2024-04-27 16:25:45 +0300

    add spreadsheet data types

--- src/types/chart.ts
+++ src/types/chart.ts
@@ -1,1 +1,11 @@
-export type DataEntry = { label: string; value: string };
+export type PatientEntry = {
+	barrier_id: string;
+	patient_id: string;
+	tract_fips: number;
+	first_name: string;
+	last_name: string;
+};
+export type HbsEntry = {
+	barrier_id: string;
+	barrier_name: string;
+};
