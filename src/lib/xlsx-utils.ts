d43c56dccba9dc8172bb5e874ac35a6f39a156b2
Hatzilu <taazrooli@gmail.com>
2024-04-27 17:22:16 +0300

    add type generics to xlsx function, fix file not being found

--- src/lib/xlsx-utils.ts
+++ src/lib/xlsx-utils.ts
@@ -1,11 +1,12 @@
 import { readFileSync } from 'fs';
+import path from 'path';
 import { readFile, utils } from 'xlsx';
 import * as XLSX from 'xlsx';
 
-export function spreadsheetToJSON(fileName: 'patients.xlsx' | 'hbs.xlsx') {
-	const a = readFileSync(`./src/data/${fileName}`);
-	// const patientsfile = path.resolve('.', 'src', 'data', 'patients.xlsx');
-	// console.log({ patientsfile });
+export function spreadsheetToJSON<T>(fileName: 'patients.xlsx' | 'hbs.xlsx') {
+	const p = path.join(process.cwd(), `src/data/${fileName}`);
+
+	const a = readFileSync(p);
 	const workbook = XLSX.read(a);
 
 	// Get the first sheet
@@ -13,7 +14,7 @@ export function spreadsheetToJSON(fileName: 'patients.xlsx' | 'hbs.xlsx') {
 	const sheet = workbook.Sheets[sheetName];
 
 	// Convert the sheet to JSON
-	const data = XLSX.utils.sheet_to_json(sheet);
+	const data = XLSX.utils.sheet_to_json<T>(sheet);
 
 	return data;
 }
