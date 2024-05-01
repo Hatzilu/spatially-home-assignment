7ee7cde6f031e3e56defabda7c978183b281e7d9
Hatzilu <taazrooli@gmail.com>
2024-05-01 18:44:53 +0300

    return error if request fails in util function

--- src/lib/fetch-utils.ts
+++ src/lib/fetch-utils.ts
@@ -5,7 +5,9 @@ import { BASE_URL } from '../../consts/consts';
 // I wrote this wrapper mostly to get type declarations for the data i was retreiving from the server.
 export async function getChartData(): Promise<ChartData[] | APIError> {
 	const res = await fetch(`${BASE_URL}/api/pieChartData`);
-
+	if (!res.ok) {
+		return { error: res.statusText };
+	}
 	const json = res.json();
 
 	return json;
